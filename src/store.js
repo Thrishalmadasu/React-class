9ea6b3c3ddebc74e170eff24f7a1fec1578592a8
Thrishalmadasu <thrishalmadasu@gmail.com>
2024-06-24 10:17:53 +0530

    Added Redux

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+function cartReducer(state={items : {}},action){
+    switch(action.type){
+        case "ADD_TO_CART" :{
+
+        }
+        case "REMOVE_FROM _CART":{
+
+        }
+        default :
+            return state;
+    }
+}
+
+const store = createStore(cartReducer);
+export default store;
